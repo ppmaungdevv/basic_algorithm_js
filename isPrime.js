81c83744df21192baab2c50c5bbadb592be908d5
ppmaungdevv <ppmaungdev@gmail.com>
2023-08-31 11:23:58 +0630

    isPrime

---
+++ isPrime.js
@@ -0,0 +1,14 @@
+// is a prime number, prime number starts from 2
+function isPrime(n) {
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(isPrime(3));
